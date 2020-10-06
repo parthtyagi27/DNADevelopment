f7950fdcbba5f8bccc9eb8922577a024f2860220
Daniel Qian <danielqian2015@gmail.com>
2020-10-06 00:46:46 -0500

    Close to the solution

--- run.cpp
+++ run.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "DNA.h"
 
 using std::cout;
@@ -34,6 +35,14 @@ int checkMatch(std::string string_l[], std::string check) {
     return -1;
 }
 
+int findDiff(int values[], int count[]) {
+    int result = 0;
+    for (int i = 0; i < sizeof(values); i++) {
+        result += abs(values[i] - count[i]);
+    }
+    return result;
+}
+
 std::string findMatch() {
     return NULL;
 }
@@ -41,7 +50,7 @@ std::string findMatch() {
 int main() {
     //first test should be Alice
     std::string test = "AGACGGGTTACCATGACTATCTATCTATCTATCTATCTATCTATCTATCACGTACGTACGTATCGAGATAGATAGATAGATAGATCCTCGACTTCGATCGCAATGAATGCCAATAGACAAAA";
-
+    //std::string test = "AACCCTGCGCGCGCGCGATCTATCTATCTATCTATCCAGCATTAGCTAGCATCAAGATAGATAGATGAATTTCGAAATGAATGAATGAATGAATGAATGAATG";
     int count[] = {0, 0, 0};
     int a_diff, b_diff, c_diff = 0;
 
@@ -51,7 +60,7 @@ int main() {
     int max = 0;
     int same = 0;
     for (int j = 0; j < 4; j++) {
-        for (int i = j; i < (int)test.length() - 3; i+=4) {
+        for (int i = j; i < (int)test.length(); i+=4) {
             temp = "";
             temp = test.substr(i, 4);
             same = checkMatch(str_list, temp);
@@ -67,7 +76,19 @@ int main() {
             prev = same;
         }
     }
+    a_diff = findDiff(Alice, count);
+    b_diff = findDiff(Bob, count);
+    c_diff = findDiff(Charlie, count);
+    if (a_diff < b_diff && a_diff < c_diff) {
+        cout << "Alice" << endl;
+    } else if (b_diff < a_diff && b_diff < c_diff) {
+        cout << "Bob" << endl;
+    } else {
+        cout << "Charlie" << endl;
+    }
+
     for (int i = 0; i < 3; i++) {
         std::cout << count[i] << std::endl;
-    }   
+    }  
+    return 0; 
 }
