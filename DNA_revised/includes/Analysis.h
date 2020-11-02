3508b46b1b4ddd23e1d056f3ea75e8c8bea1466b
parth <parth.27@outlook.com>
2020-11-02 15:39:36 -0600

    Added documentation to header files

--- DNA_revised/includes/Analysis.h
+++ DNA_revised/includes/Analysis.h
@@ -7,11 +7,40 @@
 
 class Analysis {
     public:
+        /**
+         *  Constructor for Analysis class
+         * @param given_people A vector of people to analyze 
+         */
         Analysis(std::vector<Person> given_people);
 
+        /**
+         * Check if the DNA map belongs to a particular person
+         * @param person The indivdual to check
+         * @param count The map of possible STRs and occurrences
+         * @return true if the map belongs to the person
+         */ 
         bool find_diff(Person person, std::map<std::string, int> count);
+
+        /**
+         * Find which person the given map of DNA and occurences belongs to
+         * @param count The map of DNA and occurences
+         * @return The name which the person the map belongs to 
+         */
         std::string find_match(std::map<std::string, int> count);
+
+        /**
+         * Create a map of STRs and their occurences
+         * @param strand The string of DNA to analyze
+         * @return a map of STRs and their occurences
+         */
         std::map<std::string, int> find_count(std::string strand);
+        
+        /**
+         * Find which index a particular string occurs in a vector
+         * @param string_list A vector of strings
+         * @param check The string to check
+         * @return The index at which the string occurs
+         */
         int check_match(std::vector<std::string> string_list, std::string check);
         
     private:
--- DNA_revised/includes/Person.h
+++ DNA_revised/includes/Person.h
@@ -5,9 +5,25 @@
 
 class Person {
     public:
+        /**
+        * Default constructor
+        */
         Person();
+        /**
+         * Custom constructor
+         * @param v A map of DNA STRS as the key and their occurence as the value 
+         * @param name Name of person
+        */
         Person(std::map<std::string, int> v, std::string name);
+        /**
+         * Return the name of the person
+         * @return name
+        */
         std::string get_name();
+        /**
+         * Return the map containing STR's and their occurences
+         * @return map containing STR's and their occurences
+        */
         std::map<std::string, int> get_values();
     private:
         std::map<std::string, int> str_map;
