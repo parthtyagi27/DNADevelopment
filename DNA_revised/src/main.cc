61cd8f3e40a00847d645c39f696a2a878fddea85
parth <parth.27@outlook.com>
2020-11-03 22:37:48 -0600

    Shifted code to use the new CSV reader interface

--- DNA_revised/src/main.cc
+++ DNA_revised/src/main.cc
@@ -1,55 +1,17 @@
 #include "../includes/Analysis.h"
 #include "../includes/Person.h"
 #include "../includes/DNA.h"
+#include "../includes/CSVReader.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-
-std::vector<std::string> split_line(std::string input, char delimiter) {
-    std::vector<std::string> data;
-    size_t pos = 0;
-    std::string token;
-
-    while ((pos = input.find(delimiter)) != std::string::npos) {
-        token = input.substr(0, pos);
-        data.push_back(token);
-        input.erase(0, pos + 1);
-    }
-    data.push_back(input);
-    return data;
-}
-
-std::vector<Person> csvReader(std::fstream input_file) {
-    std::string line;
-    std::string attribute_line;
-
-    std::getline(input_file, attribute_line);
-    std::vector<std::string> attributes = split_line(attribute_line, ',');
-
-    std::vector<std::string> strs;
-    strs.assign(attributes.begin() + 1, attributes.end());
-    
-    std::vector<Person> people;
-    while (std::getline(input_file, line)) {
-
-        std::vector<std::string> values = split_line(line, ',');
-        std::map<std::string, int> map;
-        for (size_t i = 1; i < values.size(); i++)
-            map.insert(std::pair<std::string, int>(attributes[i], std::stoi(values[i])));
-        Person p = Person(map, values[0]);
-        people.push_back(p);
-        for (auto it = map.begin(); it != map.end(); ++it)
-            std::cout << (*it).first << " , " << (*it).second << "\n";
-    }
-    return people;
-}
 
 int main(int argc, char * argv[]) {
     if (argc < 3) {
         std::cout << "ERROR: Please specify CSV file as the first argument and the DNA sequence as the second argument\n";
         return -1;
     }
-    std::fstream input_file = std::fstream(argv[1]);
+    // std::fstream input_file = std::fstream(argv[1]);
 
     // std::string line;
     // std::string attribute_line;
@@ -71,11 +33,11 @@ int main(int argc, char * argv[]) {
     //     for (auto it = map.begin(); it != map.end(); ++it)
     //         std::cout << (*it).first << " , " << (*it).second << "\n";
     // }
-    std::vector<Person> people = csvReader(std::fstream(argv[1]));
-    input_file.close();
+    // std::vector<Person> people = csvReader(std::fstream(argv[1]));
+    // input_file.close();
     
-    Analysis analyze(people);
-    std::map<std::string, int> test = analyze.find_count(argv[2]);
+    // Analysis analyze(people);
+    // std::map<std::string, int> test = analyze.find_count(argv[2]);
     //std::cout << test.find([""]);
     // std::unordered_map<std::string, int> parth_map;
     // parth_map.insert(std::pair<std::string, int>("AGAT", 3));
@@ -87,4 +49,12 @@ int main(int argc, char * argv[]) {
     // std::vector<Person> vec = {parth, daniel};
 
     // Analysis analyzer = Analysis(vec);
+
+    std::vector<Person> people = CSVReader::process_file(argv[1]);
+    for (Person & p : people) {
+        std::cout << p.get_name() << "\n";\
+        std::map<std::string, int> map = p.get_values();
+        for (auto it = map.begin(); it != map.end(); ++it)
+            std::cout << it->first << " , " << it->second << "\n";
+    }
 }
