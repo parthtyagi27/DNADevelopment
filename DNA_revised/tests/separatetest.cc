327b5f6c76816b69d0464481d3684981c23a65c5
parth <parth.27@outlook.com>
2020-11-04 13:33:35 -0600

    Added more test cases

--- DNA_revised/tests/separatetest.cc
+++ DNA_revised/tests/separatetest.cc
@@ -1,6 +1,93 @@
 #include "catch.hpp"
 #include "../includes/DNA.h"
 #include "../includes/Analysis.h"
+#include "../includes/CSVReader.h"
+
+TEST_CASE("CSVReader get_person works", "[weight=1]") {
+    std::vector<std::string> strs = {"AGAT", "TAGC", "AACT"};
+    std::string line = "Michael,1,2,3";
+    std::map<std::string, int> map;
+    map["AGAT"] = 1;
+    map["TAGC"] = 2;
+    map["AACT"] = 3;
+    Person p = Person(map, "Michael");
+    Person generatedPerson = CSVReader::get_person(line, strs);
+    if (p.get_name() != generatedPerson.get_name())
+        FAIL("CSVReader -- get_person fails to set name properly");
+    if (p.get_values() != generatedPerson.get_values())
+        FAIL("CSVReader -- get_person fails to set value map properly");
+    REQUIRE(true);
+}
+
+TEST_CASE("CSVReader get_person works 2", "[weight=1]") {
+    std::vector<std::string> strs = {"AGAT", "TAGC", "AACT", "GACA", "CCTG", "ATGC"};
+    std::string line = "Michael,1,2,3,4,5,6";
+    std::map<std::string, int> map;
+    map["AGAT"] = 1;
+    map["TAGC"] = 2;
+    map["AACT"] = 3;
+    map["GACA"] = 4;
+    map["CCTG"] = 5;
+    map["ATGC"] = 6;
+    Person p = Person(map, "Michael");
+    Person generatedPerson = CSVReader::get_person(line, strs);
+    if (p.get_name() != generatedPerson.get_name())
+        FAIL("CSVReader -- get_person fails to set name properly");
+    if (p.get_values() != generatedPerson.get_values())
+        FAIL("CSVReader -- get_person fails to set value map properly");
+    REQUIRE(true);
+}
+
+TEST_CASE("CSVReader process_file works", "[weight=1]") {
+    std::vector<std::string> strs = {"AGTC","TGCA","TGCT","GCAT","GGAT"};
+    std::vector<std::string> names = {"Parth", "Daniel", "Michael"};
+    int counter = 1;
+    std::vector<Person> desired_result;
+
+    for (const std::string & name : names) {
+        std::map<std::string, int> map;
+        for (size_t i  = 0; i < strs.size(); i++) {
+            map[strs[i]] = counter * (i + 1);
+        }
+        desired_result.push_back(Person(map, name));
+        counter++;
+    } 
+
+    std::vector<Person> result = CSVReader::process_file("tests/tests_part1_1.csv");
+    for (size_t i = 0; i < names.size(); i++) {
+        if (result[i].get_name() != desired_result[i].get_name())
+            FAIL("CSVReader -- process_file fails to read name properly");
+        if (result[i].get_values() != desired_result[i].get_values()) 
+            FAIL("CSVReader -- process_file fails to read STR values properly");
+    }
+    REQUIRE(true);
+}
+
+TEST_CASE("CSVReader process_file works 2", "[weight=1]") {
+    std::vector<std::string> strs = {"AGTC","TGCA","TGCT","GCAT","GGAT","CCAT","CGAT","ATGG","ATCG","CCCC","AAAG","TTGA","TCTC","CAAT","AATC"};
+    std::vector<std::string> names = {"Parth", "Daniel", "Michael", "John", "Ben"};
+    int counter = 1;
+    std::vector<Person> desired_result;
+
+    for (const std::string & name : names) {
+        std::map<std::string, int> map;
+        for (size_t i  = 0; i < strs.size(); i++) {
+            map[strs[i]] = counter * (i + 1);
+        }
+        desired_result.push_back(Person(map, name));
+        counter++;
+    } 
+
+    std::vector<Person> result = CSVReader::process_file("tests/tests_part1_2.csv");
+    for (size_t i = 0; i < names.size(); i++) {
+        if (result[i].get_name() != desired_result[i].get_name())
+            FAIL("CSVReader -- process_file fails to read name properly");
+        if (result[i].get_values() != desired_result[i].get_values()) 
+            FAIL("CSVReader -- process_file fails to read STR values properly");
+    }
+    REQUIRE(true);
+}
+
 
 TEST_CASE("Basic Person object works", "[weight=1]") {
     std::map<std::string, int> values;
@@ -63,28 +150,28 @@ TEST_CASE("Analysis -- basic find_count works", "[weight=1]") {
     REQUIRE(analysis.find_count(sequence) == result);
 }
 
-TEST_CASE("Analysis -- basic find_count 2 works", "[weight=1]") {
-    std::vector<std::string> strs = {"ACAT", "AGAT", "CCGT"};
-    std::map<std::string, int> map;
-    int counter = 0;
-    for (const std::string & i : strs)
-        map.insert(std::pair<std::string, int>(i, counter++));
-    Person p(map, "Michael");
-    std::vector<Person> vec = {p};
-    Analysis analysis(vec);
+// TEST_CASE("Analysis -- basic find_count 2 works", "[weight=1]") {
+//     std::vector<std::string> strs = {"ACAT", "AGAT", "CCGT"};
+//     std::map<std::string, int> map;
+//     int counter = 0;
+//     for (const std::string & i : strs)
+//         map.insert(std::pair<std::string, int>(i, counter++));
+//     Person p(map, "Michael");
+//     std::vector<Person> vec = {p};
+//     Analysis analysis(vec);
     
-    std::string sequence = "ACATACATAGATCCGTTAGATAGAGACG";
-    std::map<std::string, int> result;
-    for (size_t i = 0; i < strs.size(); i++) {
-        result.insert(std::pair<std::string, int>(strs[i], 1));
-    } 
-    result["ACAT"] = 2;
-    for (auto it = result.begin(); it != result.end(); ++it)
-        std::cout << it->first  << " , " << it->second << "\n";
+//     std::string sequence = "ACATACATAGATCCGTTAGATAGAGACG";
+//     std::map<std::string, int> result;
+//     for (size_t i = 0; i < strs.size(); i++) {
+//         result.insert(std::pair<std::string, int>(strs[i], 1));
+//     } 
+//     result["ACAT"] = 2;
+//     for (auto it = result.begin(); it != result.end(); ++it)
+//         std::cout << it->first  << " , " << it->second << "\n";
 
-    std::map<std::string, int> returned = analysis.find_count(sequence);
+//     std::map<std::string, int> returned = analysis.find_count(sequence);
 
-    for (auto it = returned.begin(); it != returned.end(); ++it)
-        std::cout << it->first  << " , " << it->second << "\n";
-    REQUIRE(analysis.find_count(sequence) == result);
-}
+//     for (auto it = returned.begin(); it != returned.end(); ++it)
+//         std::cout << it->first  << " , " << it->second << "\n";
+//     REQUIRE(analysis.find_count(sequence) == result);
+// }
